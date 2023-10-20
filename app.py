2027a9a68464ca11895dc0c61363dec072d9cb03
jran100 <100c.jran@gmail.com>
2023-10-20 17:17:30 +0900

    Add files via upload
    10분 이내 영상만 리스트에 뽑아주기

--- app.py
+++ app.py
@@ -5,14 +5,19 @@ import googleapiclient.discovery
 from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
-
-# YouTube API 키 설정
 api_key = "AIzaSyCcu40FYJarmjUNyilOh4gLPab8DSEOeno"  # 여기에 유튜브 API 키를 삽입
-
-# YouTube API 클라이언트 생성
 youtube = build("youtube", "v3", developerKey=api_key)
 
-# 뷰 함수: 비디오 목록을 가져와 템플릿에 전달
+def get_minute(str:str):
+    str = str[2:]
+    length = len(str)
+
+    for i in range(length):
+        if 'M' == str[i]:
+            return int(str[:i])
+        
+    return 0
+
 @app.route('/')
 def video_list():
     channel_id = "UCF4Wxdo3inmxP-Y59wXDsFw"  # MBC 뉴스 채널 ID
@@ -24,18 +29,20 @@ def video_list():
         title = youtube.videos().list(part="snippet", id=video_id).execute()
         video_title = title["items"][0]["snippet"]["title"]
         video_thumbnail = title["items"][0]["snippet"]["thumbnails"]["default"]["url"]
-        video_info.append({"id": video_id, "title": video_title, "thumbnail": video_thumbnail, "url": f"/play_video/{video_id}"})
+        video_details = youtube.videos().list(part="contentDetails", id=video_id).execute()
+        video_duration = video_details["items"][0]["contentDetails"]["duration"]
 
-    
-    return render_template('videos.html', video_info=video_info)
+        if 'H' not in video_duration:
+            minutes = get_minute(video_duration)
+            if minutes <= 10:
+                video_info.append({"id": video_id, "title": video_title, "thumbnail": video_thumbnail, "url": f"/play_video/{video_id}"})
+
+    return render_template("videos.html", video_info=video_info)
 
 @app.route("/gpt", methods=['GET'])
 def get_video_transcript():
     video_id = request.args.get('video_id')
     
-    '''
-    video id를 바탕으로 해당 동영상의 자막을 가져오는 함수
-    '''
     transcript = []
     try:
         tsAPI = YouTubeTranscriptApi.get_transcript(video_id=video_id, languages=["ko"])
